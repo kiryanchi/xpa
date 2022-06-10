d61d513bffa798789f3e8b21d90b751cef6f5734
kiryanchi <kiryanchi@naver.com>
2022-06-10 11:11:08 +0900

    삭제 구현

--- src/components/work/list/workList.py
+++ src/components/work/list/workList.py
@@ -1,6 +1,6 @@
 from PySide6 import QtGui
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QListWidget, QListWidgetItem
+from PySide6.QtWidgets import QListWidget, QListWidgetItem, QMenu
 
 from src.tools.log import Log
 
@@ -10,9 +10,22 @@ class WorkList(QListWidget):
         super().__init__()
         self.work = work
 
-        self.itemClicked.connect(self.foo)
+        self.itemClicked.connect(self.handleListWidgetClicked)
 
-    def foo(self):
+    def contextMenuEvent(self, event):
+        self.handleListWidgetClicked()
+        menu = QMenu(self)
+        delete = menu.addAction('삭제')
+
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == delete:
+            self.takeItem(self.currentRow())
+            self.work.workStacked.removeWidget(self.work.workStacked.currentWidget())
+            print(f"delete {self.currentRow()}")
+
+
+    def handleListWidgetClicked(self):
         self.work.workStacked.setCurrentIndex(self.currentRow())
 
     def addWork(self, excel):
